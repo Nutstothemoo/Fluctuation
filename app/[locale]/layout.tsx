c5bf706bf33374353bb7443af566344710842cea
maxime <maxime.dupin@topchrono.fr>
2024-04-10 17:06:12 +0200

    Add flag-icons CSS and remove i18n.ts

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -10,7 +10,7 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from '../theme-providers'
 import { Metadata } from 'next'
-
+import 'node_modules/flag-icons/css/flag-icons.min.css'
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
   display: 'swap',
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,17 +1,17 @@
 import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import Main from './Main'
+
 import SplashScreen from '@/components/splashscreen'
-import initTranslations from '../i18n'
+// import initTranslations from './i18n'
 
 export default async function Page({ params: { locale } }) {
-  const { t } = await initTranslations(locale, ['home'])
+  // const { t } = await initTranslations(locale, ['home'])
   const sortedPosts = sortPosts(allBlogs)
   const posts = allCoreContent(sortedPosts)
   return (
     <>
       <SplashScreen />
-      <h1>{t('header')}</h1>
       <Main posts={posts} />
     </>
   )
