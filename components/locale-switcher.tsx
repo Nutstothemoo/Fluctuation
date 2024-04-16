2190e1452344e24164820d5a18e8889b2f66c4ca
maxime <maxime.dupin@topchrono.fr>
2024-04-16 09:16:47 +0200

    Refactor LocaleSwitcher component in locale-switcher.tsx

--- components/locale-switcher.tsx
+++ components/locale-switcher.tsx
@@ -9,10 +9,8 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from '@/components/ui/dropdown-menu'
 
 export default function LocaleSwitcher({ locale }) {
   const [isOpen, setIsOpen] = useState(false)
@@ -29,39 +27,39 @@ export default function LocaleSwitcher({ locale }) {
 
   return (
     <div>
-          <DropdownMenu>
-          <DropdownMenuTrigger>              
-            <CountryFlag
-                    onClick={toggleDropdown}
-                    className="rounded-3xl "
-                    style={{ width: '25px', height: '25px' }}
-                    countryCode={locale === 'en' ? 'GB' : locale}
-                    svg
-                  />
-          </DropdownMenuTrigger>
-      <DropdownMenuContent className="rounded-2xl">
-        {i18n.locales
-                  .filter((local) => local !== locale)
-                  .map((local) => {
-                    return (
-                        <DropdownMenuItem
-                          className={
-                            'flex h-full w-full select-none flex-col justify-end rounded-md p-2 no-underline outline-none focus:shadow-md'
-                          }
-                        >
-                          <Link href={redirectedPathName(local)}>
-                            <CountryFlag
-                              className="rounded-full"
-                              style={{ width: '25px', height: '25px' }}
-                              countryCode={local === 'en' ? 'GB' : local}
-                              svg
-                            />
-                          </Link>
-                        </DropdownMenuItem>
-                    )
-                  })}
-      </DropdownMenuContent>
-    </DropdownMenu>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <CountryFlag
+            onClick={toggleDropdown}
+            className="rounded-3xl "
+            style={{ width: '25px', height: '25px' }}
+            countryCode={locale === 'en' ? 'GB' : locale}
+            svg
+          />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="rounded-2xl">
+          {i18n.locales
+            .filter((local) => local !== locale)
+            .map((local) => {
+              return (
+                <DropdownMenuItem
+                  className={
+                    'flex h-full w-full select-none flex-col justify-end rounded-md p-2 no-underline outline-none focus:shadow-md'
+                  }
+                >
+                  <Link href={redirectedPathName(local)}>
+                    <CountryFlag
+                      className="rounded-full"
+                      style={{ width: '25px', height: '25px' }}
+                      countryCode={local === 'en' ? 'GB' : local}
+                      svg
+                    />
+                  </Link>
+                </DropdownMenuItem>
+              )
+            })}
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   )
 }
