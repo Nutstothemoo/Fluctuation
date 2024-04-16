d595681d642d174dd8d8f94b14c32130fd837d4d
maxime <maxime.dupin@topchrono.fr>
2024-04-16 09:14:56 +0200

    Update header component to use new logo image

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,9 +8,9 @@ import ThemeSwitch from './ThemeSwitch'
 // import SearchButton from './SearchButton'
 import LocaleSwitcher from './locale-switcher'
 import Image from 'next/image'
-import soleilcouchant from '../public/assets/soleilcouchant.png'
+import logofluctuation from '../public/assets/logofluctuation.png'
 
-const Header = ({ locale , fluidFont}) => {
+const Header = ({ locale, fluidFont }) => {
   // const [scrollPosition, setScrollPosition] = useState(0)
 
   // useEffect(() => {
@@ -26,50 +26,55 @@ const Header = ({ locale , fluidFont}) => {
   // }, [])
   return (
     <>
-    <header
-      className={`
+      <header
+        className={`
           sticky top-0 z-50 flex items-center justify-between py-8 transition-all duration-500 ease-in-out`}
-    >
-      <div >
-        <Link href={`/${locale}/`} aria-label={siteMetadata.headerTitle}>
-          <div className="flex items-center justify-between">
-            <div>
+      >
+        <div>
+          <Link href={`/${locale}/`} aria-label={siteMetadata.headerTitle}>
+            <div className="flex items-center justify-between">
+              <div>
                 <Image
-                  src={soleilcouchant}
-                  alt="Picture of the author"
+                  src={logofluctuation}
+                  alt="Logo Fluctuation"
+                  // layout="fill"
+                  objectFit="contain"
                   width={70}
                   height={70}
                 />
-            </div>
-            {typeof siteMetadata.headerTitle === 'string' ? (
-                <div className={`text-indigo-950 dark:text-white dark: opacity-80 pb-10 pl-3 pt-1 pr-3 rounded-full dark: bg-indigo-950 hidden sm:block h-6 text-3xl ${fluidFont.className}`}>{siteMetadata.headerTitle}
+              </div>
+              {typeof siteMetadata.headerTitle === 'string' ? (
+                <div
+                  className={`dark: dark: hidden h-6 text-3xl text-indigo-950 dark:text-white sm:block ${fluidFont.className}`}
+                >
+                  {siteMetadata.headerTitle}
                 </div>
-            ) : (
-              siteMetadata.headerTitle
-            )}
-          </div>
-        </Link>
-      </div>    
-      <div className="p-2 px-4 opacity-80 light: bg-white dark:bg-indigo-950 rounded-full border-solid flex items-center space-x-2 lg:space-x-4 leading-5 ">
-        {headerNavLinks
-          .filter((link) => link.href !== `/${locale}/`)
-          .map((link) => (
-            <Link
-              key={link.title}
-              href={link.href}
-              className="relative hidden w-fit font-medium text-gray-900 after:absolute after:block after:h-[3px] after:w-full after:origin-center after:scale-x-0 after:bg-black after:transition after:duration-300 after:content-[''] after:hover:scale-x-100 dark:text-gray-100 dark:after:bg-white sm:block"
-            >
-              {link.title}
-            </Link>
-          ))}
-        {/* <SearchButton /> */}
-        <div className="relative z-10 flex flex-row space-x-4">
-          <LocaleSwitcher locale={locale} />
-                    <ThemeSwitch />
-          <MobileNav />
+              ) : (
+                siteMetadata.headerTitle
+              )}
+            </div>
+          </Link>
         </div>
-      </div>
-    </header>
+        <div className="light: flex items-center space-x-2 rounded-full border-solid bg-white p-1 px-4 leading-5 opacity-80 dark:bg-indigo-950 md:p-2 lg:space-x-4 ">
+          {headerNavLinks
+            .filter((link) => link.href !== `/${locale}/`)
+            .map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className="lg:text-md relative hidden w-fit text-gray-900 after:absolute after:block after:h-[3px] after:w-full after:origin-center after:scale-x-0 after:bg-black after:transition after:duration-300 after:content-[''] after:hover:scale-x-100 dark:text-gray-100 dark:after:bg-white sm:block sm:text-sm sm:text-sm"
+              >
+                {link.title}
+              </Link>
+            ))}
+          {/* <SearchButton /> */}
+          <div className="relative z-10 flex flex-row space-x-4">
+            <LocaleSwitcher locale={locale} />
+            <ThemeSwitch />
+            <MobileNav />
+          </div>
+        </div>
+      </header>
     </>
   )
 }
