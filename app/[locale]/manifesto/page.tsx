f249ce40199478fdf60bdd99bf80a95ecafcb932
maxime <maxime.dupin@topchrono.fr>
2024-04-18 16:02:49 +0200

    Refactor import statements in postbuild.mjs, package.json, page.tsx, and rss.mjs

--- app/[locale]/manifesto/page.tsx
+++ app/[locale]/manifesto/page.tsx
@@ -1,16 +1,16 @@
-import Manifesto from '@/components/Manifesto'
-import SectionContainer from '@/components/SectionContainer'
-import AnimatedBackground from '@/components/AnimatedBackground'
+import Manifesto from "@/components/Manifesto";
+import SectionContainer from "@/components/SectionContainer";
+import AnimatedBackground from "@/components/AnimatedBackground";
 
 export default async function Page({ params: { locale } }) {
-  return (
-    <>
-      <div className="mt-48">
-        <SectionContainer>
-          <Manifesto params={{ locale }} />
-        </SectionContainer>
-        <AnimatedBackground />
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div className="mt-48">
+				<SectionContainer>
+					<Manifesto params={{ locale }} />
+				</SectionContainer>
+				<AnimatedBackground />
+			</div>
+		</>
+	);
 }
