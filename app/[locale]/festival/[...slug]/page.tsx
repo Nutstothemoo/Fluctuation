be793cfe4e11d37b29a500bb75a28715229beef5
maxime <maxime.dupin@topchrono.fr>
2024-04-24 17:53:49 +0200

    test

--- app/[locale]/festival/[...slug]/page.tsx
+++ app/[locale]/festival/[...slug]/page.tsx
@@ -1,16 +1,45 @@
-import AnimatedBackground from '@/components/AnimatedBackground'
-import BoatResidency from '@/components/BoatResidency'
-import SectionContainer from '@/components/SectionContainer'
+import AnimatedBackground from "@/components/AnimatedBackground";
+import BoatResidency from "@/components/BoatResidency";
+import SectionContainer from "@/components/SectionContainer";
+import festivalsData from "@/data/festivalData";
+import Image from "next/";
+export default function Page({
+	params,
+}: {
+	params: {
+		slug: string;
+		locale: string;
+	};
+}) {
+	const festival = festivalsData.find((festival) => festival.slug.toLowerCase() === params.slug);
+	if (!festival) {
+		return (
+			<div className="mt-48">
+				<SectionContainer>
+					<div></div>
+				</SectionContainer>
+			</div>
+		);
+	}
 
-export default async function Page({ params: { locale } }) {
-  return (
-    <>
-      <div className="mt-48">
-        <SectionContainer>
-          <BoatResidency params={{ locale }} />
-        </SectionContainer>
-        <AnimatedBackground />
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div className="mt-48">
+				<SectionContainer>
+					<div className="divide-y divide-gray-200 dark:divide-gray-700">
+						<div className="space-y-2 pb-8 pt-6 md:space-y-5">
+							<h1 className="md:leading-14 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl">
+								{festival.title}
+							</h1>
+							<p className="text-lg leading-7 text-gray-500 dark:text-gray-400">Discover events</p>
+						</div>
+						<div className="container py-12">
+							<div className="-m-4 flex flex-wrap"></div>
+						</div>
+					</div>
+				</SectionContainer>
+			</div>
+			<AnimatedBackground />
+		</>
+	);
 }
--- data/festivalData.ts
+++ data/festivalData.ts
@@ -1,5 +1,6 @@
 interface Festival {
 	title: string;
+	slug: string;
 	description: string;
 	href?: string;
 	imgSrc?: string;
@@ -8,21 +9,24 @@ interface Festival {
 const festivalsData: Festival[] = [
 	{
 		title: "Brussels",
+		slug: "brussels",
 		description: `Experience the best of electronic music at ElectroFest. With performances from top DJs and producers from around the world, ElectroFest is the ultimate destination for electronic music fans.`,
 		imgSrc: "https://i.ibb.co/zfvdRYj/belgique.jpg",
-		href: "https://ibb.co/7ymhW58",
+		href: "/festival/brussels",
 	},
 	{
 		title: "Lille",
+		slug: "lille",
 		description: `Travel back in time with SynthWave, a festival dedicated to the retro sounds of the 80s. Featuring a lineup of pioneering and modern synthwave artists, SynthWave is a must-visit for fans of synth-driven music.`,
 		imgSrc: "https://i.ibb.co/r4hKxKz/france.jpg",
-		href: "https://ibb.co/HnyLtLb",
+		href: "/festival/lille",
 	},
 	{
 		title: "Utrecht",
+		slug: "utrecht",
 		description: `Travel back in time with SynthWave, a festival dedicated to the retro sounds of the 80s. Featuring a lineup of pioneering and modern synthwave artists, SynthWave is a must-visit for fans of synth-driven music.`,
 		imgSrc: "https://i.ibb.co/stZGt7P/PaysBas.jpg",
-		href: "https://ibb.co/7ymhW58",
+		href: "/festival/utrecht",
 	},
 ];
 
