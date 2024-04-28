2b753946fddf6135d83a28e09dbc75c2b00f57ac
DUPIN <maximedupin1992@gmail.com>
2024-04-28 23:41:53 +0200

    Update styling and fix layout issues in festival page

--- components/splashscreen.tsx
+++ components/splashscreen.tsx
@@ -1,19 +1,13 @@
 'use client'
 
-import { Space_Grotesk } from 'next/font/google'
 import React from 'react'
-import Image from 'next/image'
 import AnimatedSplash from './AnimatedSplash'
 
-const space_grotesk = Space_Grotesk({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-space-grotesk',
-})
+
 const SplashScreen = ({ fluidFont }) => {
   return (
     <div
-      className={`relative top-0 flex w-screen h-screen items-center justify-center ${fluidFont.className}`}
+      className={`mx-auto relative top-0 flex w-full overflow-x-hidden h-screen items-center justify-center ${fluidFont.className}`}
     >
       <div className="flex flex-col items-end">
         <AnimatedSplash subTitle="Follow the river of change" />
