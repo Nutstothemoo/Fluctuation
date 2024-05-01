c5452c695619c83a8db48ee3ff15c500447c6a60
DUPIN <maximedupin1992@gmail.com>
2024-05-01 18:52:23 +0200

    Update styling and fix layout issues in festival page

--- components/ThirdPage.tsx
+++ components/ThirdPage.tsx
@@ -13,7 +13,7 @@ const ThirdPage = async ({ params }) => {
 
   return (
     <div className="relative top-0 flex min-h-screen w-full items-center justify-center">
-      <div className="flex flex-col items-center justify-center gap-6 sm:flex-row">
+      <div className="flex flex-col items-center justify-center md:gap-6 lg:gap-24 sm:flex-row">
      
         <div className="w-[300px] rounded-lg p-2 md:w-1/2">
           <div
@@ -23,7 +23,7 @@ const ThirdPage = async ({ params }) => {
                 <Image
                   alt={"fesitboat2"}
                   src={L05}
-                  className="object-cover object-center md:h-36 lg:h-48"
+                  className="object-cover object-center sm:h-52 lg:h-64"
                   width={544}
                   height={306}
                 />
@@ -31,21 +31,21 @@ const ThirdPage = async ({ params }) => {
               <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
                 {dictionary.titlethirdpage1}
               </h2>
-              <div className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">        
-              <p className="text-start md:text-lg lg:text-xl">{dictionary.line9} :</p>     
-              <ul className="list-disc p-6">
-                <li className="text-justify text-sm sm:text-base md:text-lg lg:text-xl">
-                  {dictionary.firstcity}
-                </li>
-                <li className="text-sm sm:text-base md:text-lg lg:text-xl">{dictionary.secondcity}</li>
-                <li className="text-sm sm:text-base md:text-lg lg:text-xl">{dictionary.thirdcity}</li>
-              </ul>
+              <div className="prose max-w-none md:h-[200px] overflow-hidden text-gray-500 dark:text-gray-400">        
+                  <p className="text-start md:text-lg lg:text-xl">{dictionary.line9} :</p>     
+                  <ul className="list-disc p-6">
+                    <li className="text-justify text-sm sm:text-base md:text-lg lg:text-xl">
+                      {dictionary.firstcity}
+                    </li>
+                    <li className="text-sm sm:text-base md:text-lg lg:text-xl">{dictionary.secondcity}</li>
+                    <li className="text-sm sm:text-base md:text-lg lg:text-xl">{dictionary.thirdcity}</li>
+                  </ul>
               </div>
             </div>
           </div>
         </div>
 
-      <div className="w-[300px] rounded-lg p-2 md:w-1/2">
+      <div className="w-[300px] min-h-full rounded-lg p-2 md:w-1/2">
           <div
             className={'h-full overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700'}
           >
@@ -53,7 +53,7 @@ const ThirdPage = async ({ params }) => {
                 <Image
                   alt={"fesitboat2"}
                   src={L05}
-                  className="object-cover object-center md:h-36 lg:h-48"
+                  className="object-cover object-center sm:h-52 lg:h-64"
                   width={544}
                   height={306}
                 />
@@ -61,8 +61,8 @@ const ThirdPage = async ({ params }) => {
               <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
                   {dictionary.titlethirdpage2}
               </h2>
-                  <div className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">
-                  <p className="text-start md:text-lg lg:text-xl">{dictionary.line10}</p>
+                  <div className="prose max-w-none md:h-[200px] text-gray-500 dark:text-gray-400">
+                  <p className="text-start md:text-md lg:text-xl">{dictionary.line10}</p>
                   </div>
             </div>
           </div>
