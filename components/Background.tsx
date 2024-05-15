888bde4d02d776bc0bfaa7eb440a2d30302b3010
maxime <maxime.dupin@topchrono.fr>
2024-05-15 17:37:40 +0200

    fix

--- components/Background.tsx
+++ components/Background.tsx
@@ -1,18 +1,17 @@
-import Image from 'next/image'
-import afficheBateausanstexte from '../public/assets/afficheBateausanstexte.jpg'
-import afficheBateusanstextwebp from '../public/assets/afficheBateausanstexte.webp'
+import Image from "next/image";
+import afficheBateusanstextwebp from "../public/assets/afficheBateausanstexte.webp";
 export default function Background() {
-  return (
-    <Image
-      alt="Fluctuation background"
-      src={afficheBateusanstextwebp}
-      placeholder="blur"
-      quality={100}
-      fill
-      sizes="100vw"
-      style={{
-        objectFit: 'cover',
-      }}
-    />
-  )
+	return (
+		<Image
+			alt="Fluctuation background"
+			src={afficheBateusanstextwebp}
+			placeholder="blur"
+			quality={100}
+			fill
+			sizes="100vw"
+			style={{
+				objectFit: "cover",
+			}}
+		/>
+	);
 }
